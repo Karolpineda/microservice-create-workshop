867cec6ed7b4ec045b09b4a401ca6e1582afde5b
Karolpineda <karolpineda@hotmail.com>
2025-02-12 13:44:35 -0500

    feat: :sparkles: Update files

--- main.go
+++ main.go
@@ -13,6 +13,9 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
 	httpSwagger "github.com/swaggo/http-swagger"
+
+	// Importamos el paquete para manejar CORS
+	"github.com/gorilla/handlers"
 )
 
 // @title Workshop API
@@ -35,12 +38,20 @@ func main() {
 	// Usar el handler de Swagger
 	r.PathPrefix("/swagger/").Handler(httpSwagger.WrapHandler)
 
+	// Configurar CORS
+	corsHandler := handlers.CORS(
+		handlers.AllowedOrigins([]string{"*"}), // Permite todas las URLs (ajustar en producción)
+		handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}), // Permitir estos métodos
+		handlers.AllowedHeaders([]string{"Content-Type", "Authorization"}), // Permitir estos headers
+		handlers.AllowCredentials(),
+	)
+
 	// Tomar el puerto desde la variable de entorno
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8097" // Valor por defecto si no existe en .env
 	}
 
-	fmt.Printf("API listening on port %s\n", port)
-	log.Fatal(http.ListenAndServe(":"+port, r))
+	fmt.Printf("API escuchando en el puerto %s\n", port)
+	log.Fatal(http.ListenAndServe(":"+port, corsHandler(r))) // Aplica el middleware CORS
 }
